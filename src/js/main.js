fd0646715f669823ffccf88286077ff5068ae059
sergio <sergiousl@yahoo.com>
2018-09-25 14:16:36 +0200

    third commit. jquery was added

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,13 @@ import $ from 'jquery';
 $(document).ready(
     function () {
         $(".my-element").on("click", function () {
-            $(this).css('background-color', 'yellow')
+            $(this).css('background-color', 'yellow').animate({
+                opacity: 0.25,
+                left: "+=50",
+                height: "toggle"
+            }, 1000, function () {
+                // Animation complete.
+            });
         })
     })
 
